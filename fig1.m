c96df9db68beb36a267b9749184ff74d9f97fe8e
Troy Sankey <sankeytms@gmail.com>
2014-05-13 01:05:31 -0700

    load control package

--- fig1.m
+++ fig1.m
@@ -1,3 +1,4 @@
+pkg load control;
 graphics_toolkit("gnuplot")
 A = (1*1)*0.001;
 B = (1*1/1 + 20*0.001/(1));
@@ -7,4 +8,4 @@ num = 1;
 den = [A B C D];
 sys = tf(num,den);
 step(sys);
-print -depsc fig1.eps
+print -depsc fig1.eps;
