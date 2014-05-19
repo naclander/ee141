326fb14b56d30718c23edcce64836dee549806c7
Troy Sankey <sankeytms@gmail.com>
2014-05-18 20:26:55 -0700

    fix graph labels, font size, removed legend
    * also removed the corresponding TODO commends

--- fig3.m
+++ fig3.m
@@ -1,9 +1,12 @@
 pkg load control;
 graphics_toolkit("gnuplot");
+
 GG = tf([1], [0.001 1.02 20 0]); %G_1G_2 Transfer Fcuntion
 F = tf([400], [1]); %K_a = 400
-N = F*GG;
+N = F * GG;
 D = 1 + N;
-Step = N/D;
-step(Step);
-print -depsc fig3.eps;
+step(N / D);
+
+ylabel "y(t)";
+legend "hide";
+print -F:20 -depsc fig3.eps;
