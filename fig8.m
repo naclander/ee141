e9d68d4865a1ae35afed08c23c7e450f0adfb446
Troy Sankey <sankeytms@gmail.com>
2014-05-25 16:11:55 -0700

    fix typo, 200 -> 220

--- fig8.m
+++ fig8.m
@@ -2,7 +2,7 @@ pkg load control;
 graphics_toolkit("gnuplot");
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
-F = tf([220], [1]) %K_a = 200
+F = tf([220], [1]) %K_a = 220
 N = G2
 D = 1 + N * G1 * F * 1
 Step = N/D
