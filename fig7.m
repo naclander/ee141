b4d43f3946b101a1870847b84cf1274b7e236847
Troy Sankey <sankeytms@gmail.com>
2014-05-26 14:44:46 -0700

    added figures 7 and 8 (side by side)

--- fig7.m
+++ fig7.m
@@ -1,10 +1,14 @@
 pkg load control;
 graphics_toolkit("gnuplot");
+
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
 F = tf([200], [1]) %K_a = 200
 N = G2
 D = 1 + N * G1 * F * 1
-Step = N/D
-step(Step)
-print -depsc fig7.eps
+step(N/D)
+
+axis([0 0.6 -0.0005 0.006]);
+ylabel "y(t)";
+legend "hide";
+print -F:20 -depsc fig7.eps
--- fig8.m
+++ fig8.m
@@ -1,10 +1,14 @@
 pkg load control;
 graphics_toolkit("gnuplot");
+
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
 F = tf([220], [1]) %K_a = 220
 N = G2
 D = 1 + N * G1 * F * 1
-Step = N/D
-step(Step)
-print -depsc fig8.eps
+step(N/D)
+
+axis([0 0.6 -0.0005 0.006]);
+ylabel "y(t)";
+legend "hide";
+print -F:20 -depsc fig8.eps
