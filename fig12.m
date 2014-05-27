b3ab8fca3402ecfacb74987e9d0d6911b8471389
Troy Sankey <sankeytms@gmail.com>
2014-05-26 20:15:57 -0700

    add bode plots for task 6b

---
+++ fig12.m
@@ -0,0 +1,16 @@
+pkg load control;
+graphics_toolkit("gnuplot");
+
+K_1 = 1000
+K_3 = K_1
+G1 = tf([1], [0.001 1])
+G2 = tf([1/20], [1/20 1 0])
+G3 = tf([1], [2.814344e-9 3.183023e-5 1])
+F = tf([K_3 K_1], [1])
+N = F * G1 * G2 * G3
+D = 1 + N
+bode(N/D)
+
+title(["K_1 = K_3 = " num2str(K_1)])
+legend "hide";
+print -F:15 -depsc fig12.eps
--- fig9.m
+++ fig9.m
@@ -6,4 +6,6 @@ step(SL);
 
 ylabel "y(t)";
 legend "hide";
+xbounds = xlim()
+set(gca, 'xtick', xbounds(1):0.0002:xbounds(2))
 print -F:20 -depsc fig9.eps;
