02427e168cf5fa63e5e8d71a15c39f931c05e6fb
Troy Sankey <sankeytms@gmail.com>
2014-05-29 02:50:11 -0700

    keep figures consistent

--- fig15.m
+++ fig15.m
@@ -1,11 +1,15 @@
 pkg load control;
 graphics_toolkit("gnuplot");
+
 H = tf([0.04 1], [1]) %K_1 = 0.04
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
 F = tf([370], [1]) %K_a = 370
 N = G2
 D = 1 + N * G1 * F * H
-Step = N/D
-step(Step)
-print -depsc fig15.eps
+step(N/D)
+
+ylabel "y(t)";
+legend "hide";
+title "Step Disturbance Response";
+print -F:20 -depsc fig15.eps
--- fig9.m
+++ fig9.m
@@ -6,6 +6,7 @@ step(G3);
 
 ylabel "y(t)";
 legend "hide";
+title "";
 xbounds = xlim()
 set(gca, 'xtick', xbounds(1):0.0002:xbounds(2))
 print -F:20 -depsc fig9.eps;
