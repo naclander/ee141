87fd6230ca195e33528556a3e23aa04b2cab6650
Nathan Aclander <naclander@linux.ucla.edu>
2014-05-28 19:08:28 -0700

    G_1 and G_2 were flipped in the matlab code!

--- fig4.m
+++ fig4.m
@@ -3,8 +3,8 @@ graphics_toolkit("gnuplot");
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
 F = tf([50], [1]) %K_a = 50
-N = G1
-D = 1 + N * G2 * F * 1
+N = G2
+D = 1 + N * G1 * F * 1
 Step = N/D
 step(Step)
 print -depsc fig4.eps
--- fig5.m
+++ fig5.m
@@ -2,9 +2,9 @@ pkg load control;
 graphics_toolkit("gnuplot");
 G1 = tf([1], [0.001 1])
 G2 = tf([1/20], [1/20 1 0])
-F = tf([400], [1]) %K_a = 50
-N = G1
-D = 1 + N * G2 * F * 1
+F = tf([400], [1]) %K_a = 400
+N = G2
+D = 1 + N * G1 * F * 1
 Step = N/D
 step(Step)
 print -depsc fig5.eps
